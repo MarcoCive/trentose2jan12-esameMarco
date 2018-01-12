9b8ae8176e55c71529136c0edab95d416f420219
Civettini Marco (ACC0106222) <marco.civettini-1@sci-b106pc129x.unitn.it>
2018-01-12 11:14:54 +0100

    aggiunto index.js

---
+++ index.js
@@ -0,0 +1,75 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+const area = require('./area');
+
+// instantiate express
+const app = express();
+
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// set our port
+var port = process.env.PORT || 8080;
+
+
+// get an instance of the express Router
+var router = express.Router();
+
+// test route to make sure everything is working
+router.get('/', function (req, res) {
+    res.json({ message: 'welcome to our api!' });
+});
+
+// route /bears
+router.route('/getArea')
+
+    .get(function (req, res) {
+        if (req.query.length != 2) {
+            res.json(-1)
+        } else {
+        var result = area(req.query.side1, req.query.side2);
+        if (result == -1) {
+            res.json(result);
+        } else {
+            res.json({'area': result });
+        }
+        }
+    });
+
+    
+
+
+app.use(function (req, res, next) {
+    console.log('Something is happening.');
+    //Enabling CORS
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Content-Type', 'application/json');
+    if (req.method == 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, DELETE');
+        return res.status(200).json({});
+    }
+    // make sure we go to the next routes
+    next();
+});
+
+// register our router on /api
+app.use('/', router);
+
+// handle invalid requests and internal error
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({ error: { message: err.message } });
+});
+
+
+app.listen(port);
+console.log('server avviato ');
