ce8c040fd39525e315bbf12386690b93239c9c70
Civettini Marco (ACC0106222) <marco.civettini-1@sci-b106pc129x.unitn.it>
2018-01-12 10:33:15 +0100

    aggiunto area.js

---
+++ area.js
@@ -0,0 +1,14 @@
+exports.getArea = ((a) => {
+    if(!Array.isArray(a)) {
+        return -1;
+    } else if(a.length != 2) {
+        return -1;
+    } else if(!(Number.isInteger(a[0]) && Number.isInteger(a[1]))) {
+        return -1;
+    } else if((a[0] < 0) || (a[1] < 0)) {
+        return -1;
+    } else {
+        return (a[0]*a[1]);
+    }
+});
+
